2e961ff731c333c1019fbb53ef51b55d7530382d
tracer-ics2017 <tracer@njuics.org>
2023-02-26 00:43:11 -0800

    >  compile 2010535 cychan Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  00:43:11 up 13:04,  3 users,  load average: 1.53, 1.36, 1.28 373939dcd07b5de3fd8fea4d773527074ed55168

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -276,7 +276,7 @@ uint32_t eval(int p,int q) {
       }
     }
     uint32_t val1 = eval(p, op - 1);
-    // printf("%d\t%d\n", val1, val2);
+    printf("%d\t%d\n", val1, val2);
     switch (tokens[op].type) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
